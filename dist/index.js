3a17849740a2fa26bc545c6f64eaba59be011dea
Uditha99 <109640405+Uditha99@users.noreply.github.com>
2023-07-07 17:34:26 +0530

    Functions

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,25 @@
 "use strict";
-console.log("tpe scriptgghdhd");
-let age = 10;
-let name1 = 'uditha';
-let animals = ['we', 'ee'];
-let vehical = ['', ''];
-let student = { name2: '', age1: 2 };
-let dog = { name: "na", breed: "ad", age: 2 };
-console.log(dog.age);
-function getAgeafter3y(age, name) {
-    if (name) {
-        console.log(`Hello ${name}`);
+function sayHello(messag) {
+    console.log(`Message : ${messag}`);
+}
+sayHello("Uditha");
+const helloIsuru = (messages) => {
+};
+const horekda = (names, Theif) => {
+    if (Theif) {
+        console.log(`${names}`);
     }
     else {
-        console.log(`Hello age ${age}`);
+        console.log(`${names}`);
     }
-    return age + 3;
+};
+horekda("ranil", true);
+horekda('anura');
+function sum(...values) {
+    let ttl = 0;
+    for (let temp of values) {
+        ttl += temp;
+    }
+    return ttl;
 }
-let age3 = getAgeafter3y(1, 'uditha');
-let numbers = [10, 20, 30];
-let Strings = ['', ''];
-Strings.push("");
-Strings.pop();
-let students = ['sama', 20];
-for (let i = 0; numbers.length > i; i++) {
-    console.log(numbers[i]);
-}
-for (let temp of numbers) {
-    console.log(temp);
-}
+console.log(sum(10, 20, 30));
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,37 @@
-console.log("tpe scriptgghdhd");
+//======>
 
-//variable---------------------------
-let age:number = 10;
-
-let name1:string = 'uditha';
-let animals:Array<string>=['we','ee']
-
-let vehical:string[]=['','']
-
-let student:{name2:string,age1:number}={name2:'',age1:2}
-
-interface Animal {
-    name:string,
-    breed:string,
-    age:number
+function sayHello(messag:string){
+    console.log(`Message : ${messag}`)
 }
 
-let dog:Animal = {name:"na",breed:"ad",age:2}
-console.log(dog.age)
+sayHello("Uditha")
 
-//functions-----------------
-function getAgeafter3y(age:number,name?:string):number{
+const helloIsuru = (messages:string)=>{
 
-    if(name){
-        console.log(`Hello ${name}`)
+}
+
+const horekda = (names : string , Theif?:boolean):void=>{
+ 
+    if(Theif){
+        console.log(`${names}`)
+
     }else{
-        console.log(`Hello age ${age}`)
+        console.log(`${names}`)
     }
-    return age+3
+}
+horekda("ranil",true)
+horekda('anura')
+
+
+///=====res  parameter
+
+function sum(...values:number[]):number{
+
+    let ttl=0
+    for(let temp of values){
+        ttl+=temp
+    }
+    return ttl
 }
 
-let age3:number=getAgeafter3y(1,'uditha')
-
-///================Arrays
-
-let numbers:number[] = [10,20,30]
-
-let Strings:Array<string> = ['','']
-
-Strings.push("")
-Strings.pop()
-
-//=========> Tuple
-let students:[string , number] = ['sama', 20]
-
-///=========> 
-for(let i =0; numbers.length>i ; i++){
-    console.log(numbers[i])
-}
-
-for(let temp of numbers){
-    console.log(temp)
-}
+console.log(sum(10,20,30))

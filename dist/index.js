938581b1043bb6ad2d68128a40f3e67af475d500
Uditha99 <109640405+Uditha99@users.noreply.github.com>
2023-07-07 17:00:59 +0530

    Arrays

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,30 @@
 "use strict";
 console.log("tpe scriptgghdhd");
+let age = 10;
+let name1 = 'uditha';
+let animals = ['we', 'ee'];
+let vehical = ['', ''];
+let student = { name2: '', age1: 2 };
+let dog = { name: "na", breed: "ad", age: 2 };
+console.log(dog.age);
+function getAgeafter3y(age, name) {
+    if (name) {
+        console.log(`Hello ${name}`);
+    }
+    else {
+        console.log(`Hello age ${age}`);
+    }
+    return age + 3;
+}
+let age3 = getAgeafter3y(1, 'uditha');
+let numbers = [10, 20, 30];
+let Strings = ['', ''];
+Strings.push("");
+Strings.pop();
+let students = ['sama', 20];
+for (let i = 0; numbers.length > i; i++) {
+    console.log(numbers[i]);
+}
+for (let temp of numbers) {
+    console.log(temp);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 console.log("tpe scriptgghdhd");
 
+//variable---------------------------
 let age:number = 10;
 
 let name1:string = 'uditha';
@@ -8,3 +9,46 @@ let animals:Array<string>=['we','ee']
 let vehical:string[]=['','']
 
 let student:{name2:string,age1:number}={name2:'',age1:2}
+
+interface Animal {
+    name:string,
+    breed:string,
+    age:number
+}
+
+let dog:Animal = {name:"na",breed:"ad",age:2}
+console.log(dog.age)
+
+//functions-----------------
+function getAgeafter3y(age:number,name?:string):number{
+
+    if(name){
+        console.log(`Hello ${name}`)
+    }else{
+        console.log(`Hello age ${age}`)
+    }
+    return age+3
+}
+
+let age3:number=getAgeafter3y(1,'uditha')
+
+///================Arrays
+
+let numbers:number[] = [10,20,30]
+
+let Strings:Array<string> = ['','']
+
+Strings.push("")
+Strings.pop()
+
+//=========> Tuple
+let students:[string , number] = ['sama', 20]
+
+///=========> 
+for(let i =0; numbers.length>i ; i++){
+    console.log(numbers[i])
+}
+
+for(let temp of numbers){
+    console.log(temp)
+}

0e4afe0d22e1ab47e10ef2786b8a8421c8d4ad59
Uditha99 <109640405+Uditha99@users.noreply.github.com>
2023-07-07 08:23:23 +0530

    variables

--- index.ts
+++
@@ -1,1 +0,0 @@
-console.log("tpe scriptgghdhd");
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("tpe scriptgghdhd");
+
+let age:number = 10;
+
+let name1:string = 'uditha';
+let animals:Array<string>=['we','ee']
+
+let vehical:string[]=['','']
+
+let student:{name2:string,age1:number}={name2:'',age1:2}
